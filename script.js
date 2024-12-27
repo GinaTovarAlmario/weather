314e960dd44816500eb40ba4b258a7ea38d513ff
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-12-27 23:35:16 +0100

    defined first structure to logic with api weather

---
+++ script.js
@@ -0,0 +1,12 @@
+console.log('Js ok!');
+
+const apiKey= "cec881978f505d7affe238e8969b488d";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=genoa";
+
+// The function makes an HTTP request to the weather API.
+// It waits for the response from the API (thanks to await).
+// After the response is received, the data can be processed further 
+
+async function checkWeather() {
+    const response = await fetch(apiUrl + `&appid=${apiKey}`);  
+}
