e0d16c4ca0caf6a589da4b0029be7ccae088fa3c
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-12-27 23:38:14 +0100

    explained every logic

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+// test connection
 console.log('Js ok!');
 
 const apiKey= "cec881978f505d7affe238e8969b488d";
@@ -8,5 +10,12 @@ const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=g
 // After the response is received, the data can be processed further 
 
 async function checkWeather() {
+
     const response = await fetch(apiUrl + `&appid=${apiKey}`);  
+
+    // Waits for the response and converts it to a JavaScript object 
+    let data = await response.json();
+
+    // Logs the parsed data
+    console.log(data);
 }
